6252e4021e00b5b7d64e5b7d82b759fe856cc073
LeahB8 <leahboyd08@gmail.com>
2019-06-10 11:35:36 +0100

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const actualToyForm = document.querySelector('.add-toy-form')
 let addToy = false
 
 // YOUR CODE HERE
@@ -15,5 +16,115 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+//------------- initialise function -------------------//
 
-// OR HERE!
+const init = () =>
+  fetchToys().then(renderToys)
+
+
+//------------- server side -------------------//
+
+const toys_url = 'http://localhost:3000/toys'
+
+const fetchToys = () =>
+  fetch(toys_url).then(resp => resp.json())
+
+
+const createToyOnServer = toy => {
+  return fetch(toys_url, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: toy.name,
+      image: toy.image,
+      likes: 0
+    })
+  }).then(resp => resp.json())
+}
+
+const increaseLikesOnServer = toy => {
+  toy.likes++
+  return fetch(toys_url + `/${toy.id}`, {
+    method: 'PATCH',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      likes: toy.likes
+    })
+  }).then(resp => resp.json())
+}
+
+const deleteToyOnServer = toy => {
+  return fetch(toys_url + `/${toy.id}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+  }).then(resp => resp.json())
+}
+
+
+//------------- client side -------------------//
+
+const toyDiv = document.querySelector('#toy-collection')
+const nameInput = document.querySelector('#name-input')
+const imageInput = document.querySelector('#image-input')
+
+
+
+const renderToys = toys => {
+  toyDiv.innerHTML = ``
+  toys.forEach(toy => renderToy(toy))
+}
+
+const renderToy = toy => {
+  const toyCard = document.createElement('div')
+  toyCard.className = 'card'
+
+  toyCard.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>${toy.likes} Likes </p>
+    <button class="like-btn">Like <3</button>
+    <button class="delete-btn">X</button>
+
+
+  `
+  toyDiv.append(toyCard)
+
+  toyCard.querySelector('.like-btn').addEventListener('click', () => {
+    increaseLikesOnServer(toy)
+      .then(toyCard.querySelector('p').innerHTML = `${toy.likes} Likes`)
+  })
+
+  toyCard.querySelector('.delete-btn').addEventListener('click', () => {
+    deleteToyOnServer(toy)
+      .then(toyCard.remove())
+  })
+
+}
+
+actualToyForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  let toy = {
+    name: nameInput.value,
+    image: imageInput.value,
+    likes: 0
+  }
+  createToyOnServer(toy)
+    .then(() => {
+      renderToy(toy)
+      actualToyForm.reset()
+    })
+
+  
+})
+
+
+
+//------------- calling initialise function -------------------//
+
+init()
